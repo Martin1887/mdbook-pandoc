dbbb4df611a08161fcb4ec9cab0768ddba4d286e
Martín Pozo <mpozo@protonmail.com>
2023-02-05 13:01:06 +0100

    refactor(assets): rename `assets/templates` into `assets/pandoc`

--- mdbook-pandoc/src/config/resources/filters.rs
+++ mdbook-pandoc/src/config/resources/filters.rs
@@ -5,13 +5,13 @@ use mdbook_pandoc_derive::pandoc_template_gen;
 use serde::{Deserialize, Serialize};
 
 /// Pandoc filters, written in the `filters` subfolder of the data directory.
-#[pandoc_template_gen("assets/templates/filters.toml")]
+#[pandoc_template_gen("assets/pandoc/filters.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocFilter {}
 
 /// Pandoc Lua filters, written in the `filters` subfolder of the data directory.
-#[pandoc_template_gen("assets/templates/lua_filters.toml")]
+#[pandoc_template_gen("assets/pandoc/lua_filters.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocLuaFilter {}
--- mdbook-pandoc/src/config/resources/templates.rs
+++ mdbook-pandoc/src/config/resources/templates.rs
@@ -5,62 +5,62 @@ use mdbook_pandoc_derive::pandoc_template_gen;
 use serde::{Deserialize, Serialize};
 
 /// Pandoc templates, written in the `templates` subfolder of the data directory.
-#[pandoc_template_gen("assets/templates/templates.toml")]
+#[pandoc_template_gen("assets/pandoc/templates.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocTemplate {}
 
 /// Pandoc files for the `syntax-definition` field, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/syntax_definition.toml")]
+#[pandoc_template_gen("assets/pandoc/syntax_definition.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocSyntaxDefinition {}
 
 /// Pandoc files for the `include-in-header` field, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/include_in_header.toml")]
+#[pandoc_template_gen("assets/pandoc/include_in_header.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocIncludeInHeader {}
 
 /// Pandoc files for the `include-before-body` field, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/include_before_body.toml")]
+#[pandoc_template_gen("assets/pandoc/include_before_body.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocIncludeBeforeBody {}
 
 /// Pandoc files after the body, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/include_after_body.toml")]
+#[pandoc_template_gen("assets/pandoc/include_after_body.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocIncludeAfterBody {}
 
 /// Pandoc CSSs for the `css` field, written in the data directory.
-#[pandoc_template_gen("assets/templates/css.toml")]
+#[pandoc_template_gen("assets/pandoc/css.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocCss {}
 
 /// Pandoc files for the `embed-font` field, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/epub_embed_fonts.toml")]
+#[pandoc_template_gen("assets/pandoc/epub_embed_fonts.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocEpubEmbedFont {}
 
 /// Pandoc CSLs, written in the `src` folder.
-#[pandoc_template_gen("assets/templates/csl.toml")]
+#[pandoc_template_gen("assets/pandoc/csl.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocCsl {}
 
 /// Pandoc reference docs, written in the data directory.
-#[pandoc_template_gen("assets/templates/reference_docs.toml")]
+#[pandoc_template_gen("assets/pandoc/reference_docs.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocReferenceDoc {}
 
 /// Pandoc misc assets files (images, CLSs, STYs, etc.), written in the
 /// `src` folder of the book.
-#[pandoc_template_gen("assets/templates/template_assets.toml")]
+#[pandoc_template_gen("assets/pandoc/template_assets.toml")]
 #[derive(Clone, Default, Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum PandocTemplateAsset {}
