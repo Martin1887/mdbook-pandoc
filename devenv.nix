2f7e612647086a059404dca6c819a4c375c6002e
Martín Pozo <mpozo@protonmail.com>
2024-09-04 18:08:03 +0200

    chore: missing packages for tests in devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -6,5 +6,22 @@
   packages = [
     pkgs.vscode-extensions.vadimcn.vscode-lldb.adapter
     pkgs.taplo
+    pkgs.mdbook
+    pkgs.mdbook-plantuml
+    pkgs.plantuml
+    pkgs.librsvg
+  ];
+  languages.texlive.enable = true;
+  languages.texlive.packages = [
+    "collection-basic"
+    "collection-latex"
+    "collection-pictures"
+    "collection-fontutils"
+    "collection-latexextra"
+    "collection-latexrecommended"
+    "collection-bibtexextra"
+    "collection-mathscience"
+    "standalone"
+    "latexmk"
   ];
 }
