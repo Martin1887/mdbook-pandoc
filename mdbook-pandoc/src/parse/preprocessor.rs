c1aefcf6264ccf6aa8cb56495197f66d95f54084
Martín Pozo <mpozo@protonmail.com>
2024-09-19 15:37:53 +0200

    refactor: rename md_events to pulldown_events

--- mdbook-pandoc/src/parse/preprocessor.rs
+++ mdbook-pandoc/src/parse/preprocessor.rs
@@ -3,10 +3,10 @@ mod headings;
 /// Internal references and paths functions.
 mod links;
 /// Process events handled by pulldown-cmark to transform headings.
-mod md_events;
+mod pulldown_events;
 #[cfg(test)]
 mod tests;
 
 pub(crate) use headings::*;
 pub(crate) use links::*;
-pub(crate) use md_events::*;
+pub(crate) use pulldown_events::*;
